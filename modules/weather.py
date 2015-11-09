e70cb6962c123536892db2ac50cc21f8388d01fd
Kayama <kayama@sum.im>
2015-11-09 15:45:42 +0200

    Add weather module

--- modules/weather.py
+++ modules/weather.py
@@ -5,17 +5,45 @@
 # Потом можно будет сделать по любому городу, передавая его id как аргумент
 
 import feedparser
+import logging
+import time, func
 
-def parse():
-    
-    url = ("dp.xml")
-    #url = ('http://informer.gismeteo.ua/rss/34504.xml')
-    feed = feedparser.parse(url)
+def parse(arguments, message):
 
-    for item in feed.entries:
-        title = str(item.title)
-        print(title[16:] + "\n" + item.description + "\n")
+    response = {'chat_id': message['chat']['id']}
+
+        try:
+
+            # Локально парсим тестовый файл. Или потом превратим его в "cache"
+            url = ("dp.xml")
+            # rss информера по Днепропетровску
+            #url = ('http://informer.gismeteo.ua/rss/34504.xml')
+            feed = feedparser.parse(url)
+
+            # Формируем результат
+            result = ["\rЗначит так, прогноз такой:"]
+
+            # Разгребаем фид
+            for item in feed.entries:
+
+                title = str(item.title)
+                string = (title[16:] + "\n" + item.description + "\n")
+                print(string)
+                result.append(string)
+
+
+        except Exception as e:
+            logging.warning("Error:" + str(e))
+            response['text'] = "Нешмогла :("
+
+
+        func.send_action(response, "typing")
+        time.sleep(1)
+
+        return response
+
 
 
 if __name__ == '__main__':
     parse()
+
